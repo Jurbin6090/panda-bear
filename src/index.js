cb013439388c4610f08880e2fccd901da82ae32b
Dillon Jett Callis <DCallis@cooksys.com>
2016-12-22 09:48:23 -0600

    Basic request to json test.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import { parseString } from 'xml2js'
+import https from 'https'
+import {parseString} from 'xml2js'
+
+https.get('https://[apikey]:x@api.bamboohr.com/api/gateway.php/cooksys/v1/employees/directory', res => {
+
+  res.setEncoding('utf8');
+
+  let rawData = '';
+  res.on('data', (chunk) => rawData += chunk);
+
+  res.on('end', () => {
+    try {
+      parseString(rawData, (err, result) => {
+
+        let employees = result.directory.employees
+
+        let emps = employees[0].employee.map(employee => {
 
 
-let xml = "<root>Hello xml2js!</root>"
+          return {
+
+            id: employee.$.id,
+
+            fields: employee.field.map(field => {
+
+              return {
+
+                id: field.$.id,
+                value: field._
+
+              }
+
+            })
+          }
+
+        })
+
+        emps.forEach(console.dir)
 
 
-parseString(xml, (err, result) => {
-  console.dir(result);
-});
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+
+
+})
+
 
 
 
